5de9c7b810561f97be55eda09b4297cf79af1ad5
imnayakshubham <imnayakshubham@gmail.com>
2024-08-19 15:07:46 +0530

    Added a Loading Container

---
+++ src/components/Loader/MainLoader.tsx
@@ -0,0 +1,10 @@
+
+export const MainLoader = () => {
+    return (
+        <div
+            className="bg-transparent h-screen flex items-center justify-center"
+        >
+            Loading....
+        </div>
+    )
+}
