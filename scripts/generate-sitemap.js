b658c0ee7db4cc53ee26859828c4deec59de5e31
imnayakshubham <imnayakshubham@gmail.com>
2024-12-01 18:11:30 +0530

    fix

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -59,7 +59,6 @@ const generateSitemap = async () => {
     const dynamicRoutes = await getDynamicRoutes();
 
     const allRoutes = [...routes, ...dynamicRoutes]
-    console.log(allRoutes)
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
